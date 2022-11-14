ca389f44b0470a51939327a49a13cf0b7e9c6a60
m <>
2022-11-14 22:30:57 +0100

    Fix build tag detection

--- build_project.sh
+++ build_project.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
+
 which git > /dev/null 2>&1
 
 if [[ $? -ne  0 ]]; then 
-  echo "git is not installed using default tag"
-  docker build -t spawner:latest -f ./spawner/Dockerfile .
-  docker build -t mock-app:latest -f ./mock-app/Dockerfile .
+  tag="latest"
+  echo "Git is not installed using default tag: $tag"
 else
-  tag=${VERSION:-$( git describe --tags --dirty --abbrev=14 | sed -E 's/-([0-9]+)-g/.\1+/' )}
-  docker build -t spawner:$tag -f ./spawner/Dockerfile .
-  docker build -t mock-app:$tag -f ./mock-app/Dockerfile .
-
-  docker tag spawner:$tag spawner:latest
-  docker tag mock-app:$tag mock-app:latest
+  # try to get the tag from the current branch
+  tag=$(git describe --tags --abbrev=0 --dirty)
+  
+  if [[ $? -ne 0 ]]; then
+    # git descibe failed set latest tag
+    echo "Can not get the tag from the current repo, setting to latest"
+    tag="latest"
+  else
+    tag="$tag-alpine"
+  fi
 fi
 
 
+docker build -t spawner:$tag -f ./spawner/Dockerfile .
+docker build -t mock-app:$tag -f ./mock-app/Dockerfile .
 
+# mark the last built as latest
+if [ $tag != "latest" ]; then
+  docker tag spawner:$tag spawner:latest
+  docker tag mock-app:$tag mock-app:latest
+fi
